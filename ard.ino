ab220c8c7362f17fe4e2b5b8868face414cd910e
elamirKad <38215271+elamirKad@users.noreply.github.com>
2019-03-19 13:00:28 +0500

    Add files via upload

---
+++ ard.ino
@@ -0,0 +1,103 @@
+#include <dht.h>
+
+dht DHT;
+
+#define outputA 6
+#define outputB 5
+
+
+#define DHT11_PIN 7 //temp and humidity
+int sensorPin = A0; //light sense
+#define  INTERVAL  1200UL           // интервал между включение/выключением светодиода (1 секунда)
+const int trigPin = 9;
+const int echoPin = 10;
+long duration;
+int distance;
+int sensorValue;
+int aLastState;
+int aState;
+int counter = 0; 
+
+const int SW_pin = 2; // digital pin connected to switch output
+const int X_pin = 1; // analog pin connected to X output
+const int Y_pin = 2; // analog pin connected to Y output
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(SW_pin, INPUT);
+  digitalWrite(SW_pin, HIGH);
+  pinMode (outputA,INPUT);
+  pinMode (outputB,INPUT);
+  Serial.begin(9600);
+  aLastState = digitalRead(outputA);
+}
+
+void loop() {
+  aState = digitalRead(outputA);
+  // put your main code here, to run repeatedly:
+
+  
+  static unsigned long previousMillis = 0;        // храним время последнего переключения светодиода
+
+  
+    if(millis() - previousMillis > INTERVAL) {
+      digitalWrite(trigPin, LOW);
+      delayMicroseconds(2);
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);
+      duration = pulseIn(echoPin, HIGH);
+      distance= duration*0.034/2;
+      Serial.println(distance);
+      
+      previousMillis = millis(); 
+      int chk = DHT.read11(DHT11_PIN);
+      Serial.print(DHT.temperature);
+      Serial.println(" C");
+      Serial.println(DHT.humidity);
+      
+      sensorValue = analogRead(sensorPin);
+      Serial.println(sensorValue);
+
+      Serial.println(digitalRead(SW_pin));
+
+
+      
+      if(analogRead(X_pin) > 500 and analogRead(X_pin) < 600 and analogRead(Y_pin) > 450 and analogRead(Y_pin) < 550){
+        //center
+        Serial.println("5");
+      }else if(analogRead(X_pin) < 100){
+        //top
+        Serial.println("8");
+      }else if(analogRead(X_pin) > 900){
+        //Bottom
+        Serial.println("2");
+      }else if(analogRead(Y_pin) < 100){
+        //right
+        Serial.println("6");
+      }
+      else if(analogRead(Y_pin) > 900){
+        //left
+        Serial.println("4");
+      }else{
+        Serial.println("0");
+      }
+
+      Serial.println(counter);
+      
+    }
+
+
+
+    if (aState != aLastState){
+      if (digitalRead(outputB) != aState) { 
+       counter ++;
+      } else {
+       counter --;
+      }
+      aLastState = aState;
+    }
+
+}
---
+++ plan_rec.py
@@ -0,0 +1,108 @@
+# USAGE
+# python classify.py --model fashion.model --labelbin mlb.pickle --image examples/example_01.jpg
+
+# import the necessary packages
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+import numpy as np
+import argparse
+import imutils
+import pickle
+import cv2
+import os
+
+
+def plan_recognize(mod = "a", lab = "a", change = True):
+        if (change):
+                model = "models/sunsys.model"
+                labels = "labels/sunsys.pickle"
+        else:
+                model = mod
+                labels = lab
+
+        # load the image
+        cap = cv2.VideoCapture(1)
+        ret, img = cap.read()
+        cv2.imwrite("1.jpg", img)
+        image = cv2.imread("1.jpg")
+        output = imutils.resize(image, width=400)
+
+        # pre-process the image for classification
+        image = cv2.resize(image, (128, 128))
+        image = image.astype("float") / 255.0
+        image = img_to_array(image)
+        image = np.expand_dims(image, axis=0)
+
+        # load the trained convolutional neural network and the multi-label
+        # binarizer
+        print("[INFO] loading network...")
+        model = load_model(model)
+        mlb = pickle.loads(open(labels, "rb").read())
+
+        # classify the input image then find the indexes of the two class
+        # labels with the *largest* probability
+        print("[INFO] classifying image...")
+        proba = model.predict(image)[0]
+        idxs = np.argsort(proba)[::-1][:1]
+        # loop over the indexes of the high confidence class labels
+        for (i, j) in enumerate(idxs):
+                # build the label and draw the label on the image
+                label = "{}: {:.2f}%".format(mlb.classes_[j], proba[j] * 100)
+                #print(label)
+                cv2.putText(output, label, (10, (i * 30) + 25), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+                #cv2.putText(output, mars, (100,25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
+        # show the probabilities for each of the individual labels
+        #for (label, p) in zip(mlb.classes_, proba):
+        #        print("{}: {:.2f}%".format(label, p * 100))
+
+        # show the output image
+        cv2.imshow("Output", output)
+        cv2.waitKey(0)
+        for (i, j) in enumerate(idxs):
+                return mlb.classes_[j]
+def plan_loadimg(img, mod = "a", lab = "a", change = True):
+        if (change):
+                model = "models/sunsys.model"
+                labels = "labels/sunsys.pickle"
+        else:
+                model = mod
+                labels = lab
+        image = cv2.imread(img)
+        output = imutils.resize(image, width=400)
+         
+        # pre-process the image for classification
+        image = cv2.resize(image, (128, 128))
+        image = image.astype("float") / 255.0
+        image = img_to_array(image)
+        image = np.expand_dims(image, axis=0)
+
+        # load the trained convolutional neural network and the multi-label
+        # binarizer
+        print("[INFO] loading network...")
+        model = load_model(model)
+        mlb = pickle.loads(open(labels, "rb").read())
+
+        # classify the input image then find the indexes of the two class
+        # labels with the *largest* probability
+        print("[INFO] classifying image...")
+        proba = model.predict(image)[0]
+        idxs = np.argsort(proba)[::-1][:1]
+
+        # loop over the indexes of the high confidence class labels
+        for (i, j) in enumerate(idxs):
+                # build the label and draw the label on the image
+                label = "{}: {:.2f}%".format(mlb.classes_[j], proba[j] * 100)
+                cv2.putText(output, label, (10, (i * 30) + 25), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
+        # show the probabilities for each of the individual labels
+        #for (label, p) in zip(mlb.classes_, proba):
+        #        print("{}: {:.2f}%".format(label, p * 100))
+
+        # show the output image
+        cv2.imshow("Output", output)
+        cv2.waitKey(0)
+        for (i, j) in enumerate(idxs):
+                return mlb.classes_[j]
